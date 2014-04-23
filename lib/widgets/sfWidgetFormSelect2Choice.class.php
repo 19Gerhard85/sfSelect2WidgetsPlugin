9b7b37bb7ff08395919b9036dc0b165a663d2ae0
19Gerhard85 <gerhard.schranz@gmail.com>
2014-04-23 11:53:14 +0200

    update

--- lib/widgets/sfWidgetFormSelect2Choice.class.php
+++ lib/widgets/sfWidgetFormSelect2Choice.class.php
@@ -29,7 +29,7 @@ class sfWidgetFormSelect2Choice extends sfWidgetFormChoice
     protected function configure($options = array(), $attributes = array())
     {
         $this->addOption('culture', sfContext::getInstance()->getUser()->getCulture());
-        $this->addOption('width', 'resolve');
+        $this->addOption('width', sfConfig::get('sf_sfSelect2Widgets_width'));
 
         parent::configure($options, $attributes);
     }
